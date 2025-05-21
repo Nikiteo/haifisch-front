d4919e8185570542be29f0d21a87fcfe33192fc2
Никита Жигилей <nzhigiley@MacBook-Air-Nikita.local>
2025-05-21 09:18:45 +0300

    fixes

--- src/lib/ymaps.ts
+++ src/lib/ymaps.ts
@@ -3,7 +3,6 @@ import ReactDom from 'react-dom'
 
 const [ymaps3React] = await Promise.all([
 	ymaps3.import('@yandex/ymaps3-reactify'),
-	ymaps3.import('@yandex/ymaps3-markers@0.0.1'),
 	ymaps3.ready,
 ])
 
@@ -15,10 +14,5 @@ export const {
 	YMapMarker,
 } = reactify.module(ymaps3)
 
-// export const { YMapDefaultMarker } = reactify.module(
-// 	await ymaps3.import('@yandex/ymaps3-markers@0.0.1')
-// )
-
-export const { YMapDefaultMarker } = await reactify.module(
-	await import('@yandex/ymaps3-default-ui-theme')
-)
+const defaultUiTheme = await import('@yandex/ymaps3-default-ui-theme')
+export const { YMapDefaultMarker } = reactify.module(defaultUiTheme)
